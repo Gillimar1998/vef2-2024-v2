3f660b283d8c84c8c7339dbe1d20eb15d83cf9f1
Gillimar1998 <124063916+Gillimar1998@users.noreply.github.com>
2024-02-20 11:10:51 +0000

    lagad sma stafsetningar villur

--- src/sql/Insert_teams.sql
+++ src/sql/Insert_teams.sql
@@ -10,3 +10,7 @@ INSERT INTO teams VALUES (9, 'Vinningshópurinn');
 INSERT INTO teams VALUES (10, 'Ósigrandi skotfólkið');
 INSERT INTO teams VALUES (11, 'Óhemjurnar');
 INSERT INTO teams VALUES (12, 'Hraðaliðið');
+
+INSERT INTO users (id, name, username, password, admin) VALUES
+(1, 'BOSS', 'BOSS', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii', true),
+(2, 'boss', 'boss', '$2a$11$pgj3.zySyFOvIQEpD7W6Aund1Tw.BFarXxgLJxLbrzIv/4Nteisii', false);
